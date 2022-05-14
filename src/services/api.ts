a4c7ef307d316f142898d04f38c719282700e5a3
caionobrex <marialoucos@hotmail.com.br>
2022-05-14 17:05:08 -0300

    baseURL env name changed on axios instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,15 @@
-import { Axios, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const api = new Axios({
-  baseURL: process.env.baseURL,
+const api: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: { 'Content-Type': 'application/json' }
 })
 
 api.interceptors.request.use((config: AxiosRequestConfig) => {
-  const token = !localStorage.getItem('token')
-  if (!token) return
+  const token = localStorage.getItem('token')
+  if (!token) return config
   if (config.headers) config.headers.Authorization = `Bearer ${token}`
+  return config
 })
 
 export default api
