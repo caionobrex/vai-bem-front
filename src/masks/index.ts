cc7fa29ef08c689566179f20d659196e2d1b1e3b
caionobrex <marialoucos@hotmail.com.br>
2022-05-15 19:36:29 -0300

    mask functions added

---
+++ src/masks/index.ts
@@ -0,0 +1,15 @@
+export const cpfMask = (value: string): string => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1')
+}
+
+export const phoneMask = (v: string): string => {
+  v = v.replace(/\D/g, '')
+  v = v.replace(/^(\d{2})(\d)/g, '($1) $2')
+  v = v.replace(/(\d)(\d{4})$/, '$1-$2')
+  return v
+}
